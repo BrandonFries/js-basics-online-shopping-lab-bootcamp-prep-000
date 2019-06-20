22148a02290c58bd86bdfaa42542c1668424a6e8
Brandon Fries <brandonfries1993@gmail.com>
2019-06-20 21:29:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ return b
    if ( cardnumber != undefined){
      var v = total()
      cart.length = 0;
-     return 'Your total cost is $${v}, which will be charged to the card ${cardnumber}.'
+     return "Your total cost is $${v}, which will be charged to the card ${cardnumber}."
    }
    else {
      return "Sorry, we don't have a credit card on file for you"
