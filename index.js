0ed338026dda6576025a1223ab97705fe794b24b
Brandon Fries <brandonfries1993@gmail.com>
2019-06-20 20:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 var cart = [];
 
-function getCart() {
- return cart;
-}
 
-function setCart(c) {
-  cart = c;
+function getCart() {
   return cart;
 }
 
+function setCart(c) {
+   cart = c;
+  return cart; 0
+}
+
+
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * 100);
+  var itemName = {itemName: itemName, itemPrice: itemPrice};
+  cart.push({
+    [item] : itemPrice,
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 100 + 1)
+  })
+  return(item + " has been added to your cart.")
+
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  var newitems = [];
+  for (var i = 0; i < cart.length;) {
+    var obj = Object.keys(cart[i])[0]
+     newitems.push( obj + " at $" + cart[i][obj])
+  }
+  var string = "In your cart, you have "
+  if (newitems.length === 1) {
+  string += newitems + "."
+} else if (newitems.length === 2) {
+  string += (newitems[0] + " and " + newitems[1] + ".")
+} else if (newitems.length > 2) {
+  var lastitem = newitems.pop()
+  var miditems = newitems.join(", ")
+  string += (miditems + ", and " + lastitem + ".")
+}
+return string
 }
 
-function total() {
-  // write your code here
-}
+ function total() {
 
-function removeFromCart(item) {
-  // write your code here
-}
+ }
 
-function placeOrder(cardNumber) {
-  // write your code here
-}
+
+ function removeFromCart(item) {
+
+ }
+
+
+ function placeOrder(cardNumber) {
+
+ }
