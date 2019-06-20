1d0afc45c6fb5e80b3077d3c0b291a893acc8ba6
Brandon Fries <brandonfries1993@gmail.com>
2019-06-20 20:56:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function addToCart(item) {
   cart.push({
     [item] : itemPrice,
     itemName: item,
-    itemPrice: Math.floor(Math.random() * 100 + 1 - 1)
+    itemPrice: Math.floor(Math.random() * 100 + )
   })
   return(item + " has been added to your cart.")
 
