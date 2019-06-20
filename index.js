54ae6591bf0d9c338c5f58e31bfb6986adbb1efe
Brandon Fries <brandonfries1993@gmail.com>
2019-06-20 21:54:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,7 +68,4 @@ return b
  function placeOrder(cardNumber) {
      if (cart.length === 0);
      return "Sorry, we dont have a credit card on file for you.";
-   } else {
-   return  'Your total cost is $' + total() + ' which will be charged to the card '+ cardNumber + '.';
-  }
- }
+   } 
